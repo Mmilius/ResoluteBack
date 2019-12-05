d2018fae64249ce898d5fd45895d0197de6062c7
github username <github email address>
2019-12-05 10:12:43 -0700

    update reports controller with delete and update

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -17,6 +17,27 @@ class ReportsController < ApplicationController
             image: params[:image],
             resolution_id: params[:resolution_id]
         })
+        render json: @report
+    end
+
+    def destroy
+        @report = Report.find(params[:id])
+        @report.destroy
+    end
+
+    def update
+        @report = Report.find(params[:id])
+        if @report.update(report_params)
+            render json: @report
+        else
+            render json: @report.errors, status: :unprocessable_entity
+        end
+
+    end
+
+    private
+    def report_params
+        params.require(:report).permit(:status, :challenges, :learned, :image, :resolution_id, :created_at, :updated_at, :id)
     end
 
 
